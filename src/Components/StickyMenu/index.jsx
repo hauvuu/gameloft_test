eee4305fadc5d64a34baafe103dc872ec3eb3963
Vũ Hậu <hauzu@Hauzus-Macbook-Pro.local>
2023-03-31 16:48:11 +0700

    sticky menu reponsive

--- src/Components/StickyMenu/index.jsx
+++ src/Components/StickyMenu/index.jsx
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 const StickyMenu = () => {
   return (
-    <div className='root fixed-top d-flex align-items-center justify-content-between'>
+    <div className='root collapse fixed-top d-flex align-items-center justify-content-between'>
       <img src='/static/icons/logo.png' alt="logo" />
       <img src='/static/icons/hamberger_meu_icon.png' alt="logo" />
     </div>
