995b51df8ce697dc2e18aff3bab05484789566ef
Vũ Hậu <hauzu@Hauzus-Macbook-Pro.local>
2023-03-31 16:26:21 +0700

    sticky menu

---
+++ src/Components/StickyMenu/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const StickyMenu = () => {
+  return (
+    <div className='root fixed-top d-flex align-items-center justify-content-between'>
+      <img src='/static/icons/logo.png' alt="logo" />
+      <img src='/static/icons/hamberger_meu_icon.png' alt="logo" />
+    </div>
+  );
+};
+
+export default StickyMenu;
