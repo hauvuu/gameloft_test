4a1c06c96b989ffdeeb47410cd7ba621bd21e902
Vũ Hậu <hauzu@Hauzus-Macbook-Pro.local>
2023-04-01 14:22:30 +0700

    Banner and reponsive banner

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import StickyMenu from './Components/StickyMenu';
+import Banner from './Components/Banner';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,8 +19,9 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
       <StickyMenu />
+      <Banner />
     </div>
   );
 }
