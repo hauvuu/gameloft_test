6629ffd8e9ff056442e9659700a8ce262620f436
Vũ Hậu <hauzu@Hauzus-Macbook-Pro.local>
2023-04-05 11:18:19 +0700

    full screen scroll, download img, link to another site

--- src/Components/GameContent/GameContentItem.jsx
+++ src/Components/GameContent/GameContentItem.jsx
@@ -1,20 +1,49 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import './GameContentItem.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import html2canvas from "html2canvas";
 
 const GameContentItem = props => {
-  const { key, img, downloadSrc } = props;
+  const { key, img } = props;
+
+  const ref = useRef(null);
+
+  const handleClickTakeScreenShot = () => {
+    const { cropPositionTop, cropPositionLeft, cropWidth, cropHeigth } = {
+      cropPositionTop: 0,
+      cropPositionLeft: 0,
+      cropWidth: 1400,
+      cropHeigth: 1800
+    };
+
+    html2canvas(ref.current).then(canvas => {
+      let croppedCanvas = document.createElement("canvas");
+      let croppedCanvasContext = croppedCanvas.getContext("2d");
+
+      croppedCanvas.width = cropWidth;
+      croppedCanvas.height = cropHeigth;
+
+      croppedCanvasContext.drawImage(canvas, cropPositionLeft, cropPositionTop);
+
+      const a = document.createElement("a");
+      a.href = croppedCanvas.toDataURL();
+      a.download = "game_img.png";
+      a.click();
+    });
+  };
 
   return (
     <div
+      id="#screenshot"
       className='GameContentItem d-flex justify-content-end align-items-start'
       style={{ backgroundImage: `url(${img ? img : 'static/images/GameContentItem_background.png'})` }}
       key={key}
+      ref={ref}
     >
       <img
         src='static/icons/GameContentItem_download_btn.png'
         alt='download button'
-        onClick={() => console.log(downloadSrc)}
+        onClick={() => handleClickTakeScreenShot()}
       />
     </div>
   );
@@ -22,8 +51,7 @@ const GameContentItem = props => {
 
 GameContentItem.defaultProps = {
   key: 0,
-  img: '',
-  downloadSrc: ''
+  img: ''
 };
 
 export default GameContentItem;
